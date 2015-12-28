059ab162425f4b37d9d44ee58fd59fa15468b5d6
Zack Scholl <zack.scholl@gmail.com>
2015-12-28 08:33:41 -0800

    Works, needs some improvements.

--- load.go
+++ load.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math/rand"
 	"os"
+	"regexp"
 	"strings"
 )
 
@@ -119,41 +121,70 @@ func getSynonyms(w string) (possibilities []string) {
 	return
 }
 
-func getHaikus(words []string) (haikus []string) {
-	gotHaiku := true
-	i := 0
+type haikuCluster struct {
+	haikus    []string
+	isHaikus  []bool
+	numHaikus int
+}
+
+func getHaikus(words []string) (haikus []string, isHaikus []bool, numHaikus int) {
+	numHaikus = 0
 	for {
-		i, gotHaiku = isHaiku(words)
+		haikuString, i, gotHaiku := isHaiku(words)
+		haikus = append(haikus, haikuString)
+		isHaikus = append(isHaikus, gotHaiku)
 		if gotHaiku == false {
 			return
+		} else {
+			numHaikus = numHaikus + 1
 		}
-		fmt.Println(i)
-		fmt.Println(gotHaiku)
-		haikus = append(haikus, strings.Join(words[:i], " "))
-		words = words[i:]
+
+		words = words[i+1:]
 	}
 }
 
-func isHaiku(words []string) (int, bool) {
+func isHaiku(words []string) (string, int, bool) {
 	checks := []int{5, 7, 5}
 	curCheck := 0
 	runningTotal := 0
-
+	currentHaiku := ""
 	for i, word := range words {
 		slbles := cmudict[word]
 		runningTotal = runningTotal + slbles
+		currentHaiku = currentHaiku + word + " "
 		if runningTotal == checks[curCheck] {
 			curCheck += 1
 			if curCheck == 3 {
-				return i + 1, true
+				return currentHaiku, i, true
+			} else {
+				currentHaiku = currentHaiku + "\n"
+
 			}
 			runningTotal = 0
 		}
-		if runningTotal > checks[curCheck] {
-			return -1, false
+	}
+	return currentHaiku, -1, false
+}
+
+func randChoices(limits []int) (choices []int) {
+	for _, l := range limits {
+		choices = append(choices, rand.Intn(l))
+	}
+	return
+}
+
+func listAlternates(input []string) (output [50][]string) {
+	totals := make([]int, len(input))
+	for i, w := range input {
+		totals[i] = len(getSynonyms(w))
+	}
+	for i := 0; i < len(output); i++ {
+		choices := randChoices(totals)
+		for j, w := range input {
+			output[i] = append(output[i], getSynonyms(w)[choices[j]])
 		}
 	}
-	return -1, false
+	return
 }
 
 func init() {
@@ -163,22 +194,54 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(thesaurus["chocolate"])
 	cmudict, err = loadCmudict("./resources/cmudict.0.7a")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(cmudict["chocolate"])
-
 	skipwords = []string{"a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also", "although", "always", "am", "among", "amongst", "amoungst", "amount", "an", "and", "another", "any", "anyhow", "anyone", "anything", "anyway", "anywhere", "are", "around", "as", "at", "back", "be", "became", "because", "become", "becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom", "but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven", "else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own", "part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the"}
 
 }
 
 func main() {
+	// words := strings.Split(`want to play a game with seventeen syllables we write some poem want to play a game with seventeen syllables we write some poem something else`, " ")
+	// fmt.Println(getHaikus(words))
+	//input := []string{"cat", "is", "nice"}
+	sentence := `This is the sixth time we have had the pleasure of writing a birthday blog post for Go, and we would not be doing so if not for the wonderful and passionate people in our community. The Go team would like to thank everyone who has contributed code, written an open source library, authored a blog post, helped a new gopher, or just given Go a try. `
+	fmt.Println("ORIGINAL:")
+	fmt.Println(sentence)
+	sentence = strings.ToLower(sentence)
+	sentence = strings.Replace(sentence, "don't", "do not", -1)
+	sentence = strings.Replace(sentence, "'", "", -1)
+	words := regexp.MustCompile(`(\w+)`).FindAllString(sentence, -1)
+	alternatives := listAlternates(words)
+	bestNum := 0
+	var bestHaikus []string
+	var bestIsHaikus []bool
 
-	fmt.Println(getSynonyms("the"))
+	for i := 0; i < len(alternatives); i++ {
+		for j := 0; j < 3; j++ {
+			haikuString, isHaikus, numHaikus := getHaikus(alternatives[i][j:])
+			if numHaikus > bestNum {
+				if j > 0 {
+					haikuString = append([]string{strings.Join(alternatives[i][:j], " ")}, haikuString...)
+					isHaikus = append([]bool{false}, isHaikus...)
+				}
+				bestHaikus = haikuString
+				bestIsHaikus = isHaikus
+			}
+		}
+	}
+	fmt.Println("\n\nBEST HAIKU:")
+	for i, bestHaiku := range bestHaikus {
+		if bestIsHaikus[i] == true {
+			fmt.Printf("\n")
+		}
+		fmt.Println(bestHaiku)
+		if bestIsHaikus[i] == true {
+			fmt.Printf("\n")
+		}
+	}
 
-	words := strings.Split(`want to play a game with seventeen syllables we write some poem want to play a game with seventeen syllables we write some poem something else`, " ")
-	fmt.Println(getHaikus(words))
+	// Todo: replace each word with the puncuation near the word in the original
 
 }
